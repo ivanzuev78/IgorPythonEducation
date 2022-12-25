aa2089a26f3488ed584a23d33d9fbb4ba2f9c228
user <jfjfjfj@.ru>
2022-12-25 14:40:07 +0300

    дз 3

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -10,3 +10,18 @@
 """
 
 list_1 = ['Мама', 'мыла', 'рыбу', 'неизвестно', 'зачем']
+
+text = ''
+text_2 = ''
+for word in list_1:
+    text += word + ' '
+    text_2 += word + '.'
+
+print(text)
+print(text_2)
+
+text_3 = ' '.join(list_1)
+print(text_3)
+
+text_3 = '.'.join(list_1)
+print(text_3)
